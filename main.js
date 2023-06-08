295f3eca786a29af2aba3ff253e30a615c0ac0ca
Deepak lak <dkumar17071@gmail.com>
2023-06-08 16:31:35 +0530

    done with  FAQs now jumping on testimonials

--- main.js
+++ main.js
@@ -2,3 +2,23 @@ window.addEventListener("scroll",()=>{
     console.log("hello")
     document.querySelector("nav").classList.toggle("window_scroll",window.scrollY>0)
 })
+
+//  FAQ section
+
+const faqs =  document.querySelectorAll(".faq")
+
+faqs.forEach(faq=>{
+    faq.addEventListener("click",()=>{
+        
+        faq.classList.toggle("open")
+         // changing iconn
+         const icon = faq.querySelector(".faq__icon i")
+         if(icon.className === "fa-solid fa-plus"){
+            icon.className = "fa-solid fa-minus"
+         } else{
+            icon.className = "fa-solid fa-plus"
+         }
+
+
+    })
+})
